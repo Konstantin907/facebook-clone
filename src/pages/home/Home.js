39423bb1b54b8a33a6cf765093a7b53780d21b22
konstantin <konstantinivanovic12345@gmail.com>
2024-01-30 20:42:40 -0800

    topbar created

---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import { Person } from "@mui/icons-material"
+import { Topbar } from "../../components/topbar/Topbar"
+
+export const Home = () => {
+  return (
+    <div>
+        <Topbar />
+    </div>
+  )
+}
