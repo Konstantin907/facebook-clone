471327b8604db0f7a5dde5ff32dd67972ba0cf20
konstantin <konstantinivanovic12345@gmail.com>
2024-02-01 18:34:09 -0800

    feed

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,38 @@
+import "./post.css"
+import {MoreVert} from "@mui/icons-material"
+
+export const Post = () => {
+  return (
+    <div className="post">
+        <div className="postWrapper">
+
+            <div className="postTop">
+                <div className="postTopLeft">
+                    <img className="postProfileImg" src="/assets/10.jpeg" alt="" />
+                    <span className="postUsername">Jane Raven</span>
+                    <span className="postDate">5 min ago</span>
+                    
+                </div>
+                <div className="postTopRight">
+                <MoreVert />
+                </div>
+            </div>
+            <div className="postCenter">
+                <span className="postText">Hey! It's my first post :hands</span>
+                <img className="postImg" src="/assets/post1.jpeg" alt="" />
+            </div>
+            <div className="postBottom">
+                <div className="postBottomLeft">
+                    <img className="likeIcon" src="/assets/like.png" alt="" />
+                    <img className="likeIcon" src="/assets/heart.png" alt="" />
+                    <span className="postLikeCounter">32 people like it</span>
+                </div>
+                <div className="postBottomRight">
+                    <span className="postCommentText">9 comments</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
